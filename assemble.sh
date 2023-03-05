7fa38bedc725c9df87f6b3151dd25772d1104ddd
9names <60134748+9names@users.noreply.github.com>
2023-03-05 21:15:00 +1100

    Add hacks to get rv32e to build

--- assemble.sh
+++ assemble.sh
@@ -7,11 +7,15 @@ crate=riscv-rt
 # remove existing blobs because otherwise this will append object files to the old blobs
 rm -f bin/*.a
 
-exts=('i' 'ic' 'im' 'imc' 'if' 'ifc' 'imf' 'imfc' 'ifd' 'ifdc' 'imfd' 'imfdc')
+# exts=('i' 'ic' 'im' 'imc' 'if' 'ifc' 'imf' 'imfc' 'ifd' 'ifdc' 'imfd' 'imfdc')
+exts=()
 
 for ext in ${exts[@]}
 do
     case $ext in
+        'e'*)
+            abi='e'
+            ;;
 
         *'d'*)
             abi='d'
@@ -38,6 +42,35 @@ do
 
     riscv64-unknown-elf-gcc -DSMODE -ggdb3 -fdebug-prefix-map=$(pwd)=/riscv-rt -c -mabi=lp64${abi} -march=rv64${ext} asm.S -o bin/$crate.o
     riscv64-unknown-elf-ar crs bin/riscv64${ext}-unknown-none-elf-smode.a bin/$crate.o
+
+done
+
+e_variants=('e' 'ec' 'em' 'emc')
+
+for ext in ${e_variants[@]}
+do
+    case $ext in
+        'e'*)
+            abi='e'
+            ;;
+        
+        *)
+            abi=''
+            ;;
+    esac
+
+    riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$(pwd)=/riscv-rt -c -mabi=ilp32${abi} -march=rv32${ext} asm_e.S -o bin/$crate.o
+    riscv64-unknown-elf-ar crs bin/riscv32${ext}-unknown-none-elf.a bin/$crate.o
+
+    # riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$(pwd)=/riscv-rt -c -mabi=lp64${abi} -march=rv64${ext} asm_e.S -o bin/$crate.o
+    # riscv64-unknown-elf-ar crs bin/riscv64${ext}-unknown-none-elf.a bin/$crate.o
+
+    #s-mode
+    # riscv64-unknown-elf-gcc -DSMODE -ggdb3 -fdebug-prefix-map=$(pwd)=/riscv-rt -c -mabi=ilp32${abi} -march=rv32${ext} asm_e.S -o bin/$crate.o
+    # riscv64-unknown-elf-ar crs bin/riscv32${ext}-unknown-none-elf-smode.a bin/$crate.o
+
+    # riscv64-unknown-elf-gcc -DSMODE -ggdb3 -fdebug-prefix-map=$(pwd)=/riscv-rt -c -mabi=lp64${abi} -march=rv64${ext} asm_e.S -o bin/$crate.o
+    # riscv64-unknown-elf-ar crs bin/riscv64${ext}-unknown-none-elf-smode.a bin/$crate.o
 
 done
 
